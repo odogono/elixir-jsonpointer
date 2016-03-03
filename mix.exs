74aaeb49e3b797974ed3e2c34ce4a3542cff7293
Alex Veenendaal <alex@opendoorgonorth.com>
2016-03-03 12:40:09 +0000

    doc support added.
    package information filled out.

--- mix.exs
+++ mix.exs
@@ -11,7 +11,7 @@ defmodule JsonPointer.Mixfile do
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps,
-     docs: [readme: "README.md", main: "README",
+     docs: [readme: "README.md",
             source_ref: "v#{@version}",
             source_url: "https://github.com/odogono/json_pointer"]]
   end
@@ -24,12 +24,23 @@ defmodule JsonPointer.Mixfile do
   end
 
   defp deps do
-    []
+    [{:earmark, "~> 0.1", only: :dev},
+      {:ex_doc, "~> 0.11", only: :dev}]
+  end
+
+  defp description do
+    """
+    An implementation of RFC 6901
+    """
   end
 
   defp package do
-    %{licenses: ["MIT"],
-      links: %{"GitHub" => "https://github.com/odogono/json_pointer"}}
+    %{name: "odgn_json_pointer",
+      licenses: ["MIT"],
+      maintainers: [ "Alexander Veenendaal" ],
+      links: %{"GitHub" => "https://github.com/odogono/json_pointer"},
+      files: [ "lib", "mix.exs", "README.md", "LICENSE"]
+    }
   end
 
 end
