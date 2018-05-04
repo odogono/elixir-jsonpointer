46a1cdbf6bd857706568a4ba2907204d3745805d
Mike Stok <mike@stok.ca>
2018-05-04 09:56:51 -0400

    Make all defguard → defguardp to clean up docs.
    This stops the guards appearing in the documentation generated by `mix
    docs`.

--- lib/json_pointer.ex
+++ lib/json_pointer.ex
@@ -11,45 +11,45 @@ defmodule JSONPointer do
   @typep transform_fn :: ((any()) -> any())
   @typep apply_mapping :: { String.t(), String.t() } | { String.t(), String.t(), transform_fn } | { String.t(), (() -> any()) }
 
-  defguard is_remove_from_map(operation, map, tokens)
-           when operation == :remove and tokens == [] and is_map(map)
+  defguardp is_remove_from_map(operation, map, tokens)
+            when operation == :remove and tokens == [] and is_map(map)
 
-  defguard is_remove_from_list(operation, list, tokens)
-           when operation == :remove and tokens == [] and is_list(list)
+  defguardp is_remove_from_list(operation, list, tokens)
+            when operation == :remove and tokens == [] and is_list(list)
 
-  defguard is_set_map(operation, map, tokens)
-           when operation == :set and tokens == [] and is_map(map)
+  defguardp is_set_map(operation, map, tokens)
+            when operation == :set and tokens == [] and is_map(map)
 
-  defguard is_set_list(operation, list, tokens)
-           when operation == :set and tokens == [] and is_list(list)
+  defguardp is_set_list(operation, list, tokens)
+            when operation == :set and tokens == [] and is_list(list)
 
-  defguard is_set_list_nil_child(operation, parent, tokens, child)
-           when operation == :set and tokens == [] and is_list(parent) and child == nil
+  defguardp is_set_list_nil_child(operation, parent, tokens, child)
+            when operation == :set and tokens == [] and is_list(parent) and child == nil
 
-  defguard is_get_map(operation, map, tokens)
-           when (operation == :has or operation == :get) and tokens == [] and is_map(map)
+  defguardp is_get_map(operation, map, tokens)
+            when (operation == :has or operation == :get) and tokens == [] and is_map(map)
 
-  defguard is_get_list(operation, list, tokens)
-           when (operation == :has or operation == :get) and tokens == [] and is_list(list)
+  defguardp is_get_list(operation, list, tokens)
+            when (operation == :has or operation == :get) and tokens == [] and is_list(list)
 
-  defguard is_set_map_no_tokens(operation, map)
-           when operation == :set and is_map(map)
+  defguardp is_set_map_no_tokens(operation, map)
+            when operation == :set and is_map(map)
 
-  defguard is_set_list_no_tokens(operation, list)
-           when operation == :set and is_list(list)
+  defguardp is_set_list_no_tokens(operation, list)
+            when operation == :set and is_list(list)
 
-  defguard is_set_remove_map(operation, map)
-           when (operation == :set or operation == :remove) and is_map(map)
+  defguardp is_set_remove_map(operation, map)
+            when (operation == :set or operation == :remove) and is_map(map)
 
-  defguard is_set_remove_list(operation, list)
-           when (operation == :set or operation == :remove) and is_list(list)
+  defguardp is_set_remove_list(operation, list)
+            when (operation == :set or operation == :remove) and is_list(list)
 
-  defguard is_set_nil_container(operation, container)
-           when operation == :set and container == nil
+  defguardp is_set_nil_container(operation, container)
+            when operation == :set and container == nil
 
-  defguard is_empty_map(value) when value == %{}
+  defguardp is_empty_map(value) when value == %{}
 
-  defguard is_empty_list(value) when value == []
+  defguardp is_empty_list(value) when value == []
 
   @doc """
   Retrieves the value indicated by the pointer from the object
